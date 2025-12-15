badc02386b143f239e3c163fe2d38ceac3d49756
sandula-sanchana <sandulasanchana@gmail.com>
2025-12-15 16:08:01 +0530

    import react boostrap+boostrap

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,63 +1,20 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
 export const Header = () => {
     return (
-        <header>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
-                <div className="container">
-                    {/* Brand */}
-                    <a className="navbar-brand fw-bold" href="/">
-                        MyApp
-                    </a>
-
-                    {/* Mobile toggle */}
-                    <button
-                        className="navbar-toggler"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#mainNavbar"
-                        aria-controls="mainNavbar"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-
-                    {/* Nav links */}
-                    <div className="collapse navbar-collapse" id="mainNavbar">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link active" href="/">
-                                    Home
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/about">
-                                    About
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/services">
-                                    Services
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/contact">
-                                    Contact
-                                </a>
-                            </li>
-                        </ul>
-
-                        {/* Right-side buttons */}
-                        <div className="d-flex gap-2">
-                            <a href="/login" className="btn btn-outline-light btn-sm">
-                                Login
-                            </a>
-                            <a href="/signup" className="btn btn-primary btn-sm">
-                                Sign Up
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-        </header>
+       <>
+           <Navbar bg="dark" data-bs-theme="dark">
+               <Container>
+                   <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                   <Nav className="me-auto">
+                       <Nav.Link href="#home">Home</Nav.Link>
+                       <Nav.Link href="#features">Features</Nav.Link>
+                       <Nav.Link href="#pricing">Pricing</Nav.Link>
+                   </Nav>
+               </Container>
+           </Navbar>
+       </>
     );
 };
